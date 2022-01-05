00140e79527164daa4e6441a183dd18e1acb15bc
정훈박 <fireking5997@gmail.com>
2022-01-05 21:27:52 +0900

    Add:차트 라이브러리 c3 사용해봄

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,44 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import Wrapper from "../components/Wrapper";
+import * as c3 from "c3";
+import axios from "axios";
 
 const Dashboard: FC = (props) => {
-  return <Wrapper>Dashboard</Wrapper>;
+  useEffect(() => {
+    (async () => {
+      const chart = c3.generate({
+        data: {
+          x: "X",
+          columns: [["X"], ["Sales"]],
+          types: {
+            Sales: "bar",
+          },
+        },
+        axis: {
+          x: {
+            type: "timeseries",
+            tick: {
+              format: "%Y-%m-%d",
+            },
+          },
+        },
+      });
+      const { data } = await axios.get("/orders/chart");
+      chart.load({
+        columns: [
+          ["X", ...data.map((r: any) => r.date)],
+          ["Sales", ...data.map((r: any) => Number(r.priceSum))],
+        ],
+      });
+    })();
+  }, []);
+
+  return (
+    <Wrapper>
+      <h2>Daily Sale</h2>
+      <div id="chart" />
+    </Wrapper>
+  );
 };
 
 export default Dashboard;
